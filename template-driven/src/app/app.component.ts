a16c136eb9f1625350f4a50348d9e86e0f816683
jaibwii <jbrwoj32@gmail.com>
2025-02-17 12:21:41 +0800

    forms created

--- template-driven/src/app/app.component.ts
+++ template-driven/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'template-driven';
+  name: string = "";
+  email: string = "";
+  phoneno: string ="";
+  section: string = "";
+  course: string = "";
+  queries: string = "";
+  submitted: boolean = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
 }
